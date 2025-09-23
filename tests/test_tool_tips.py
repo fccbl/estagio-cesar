c62335c641de7172cc838a10c3fbec1f4756f40b
fccbl <fccbl@EU3146015021186.local>
2025-09-23 11:57:26 -0300

    day 4

---
+++ tests/test_tool_tips.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from pages.tool_tips_page import ToolTipsPage
+import json
+
+
+def test_button_tooltip(driver, test_data):
+
+    tool_tips_page = ToolTipsPage(driver)
+    tool_tips_page.navigate(test_data["tool_tips_url"])
+
+    tool_tips_page.hover_over_button()
+
+    tooltip_text = tool_tips_page.get_tooltip_text()
+    assert tooltip_text == test_data["tool_tip_button_text"]
+
+def test_field_tooltip(driver, test_data):
+
+    tool_tips_page = ToolTipsPage(driver)
+
+    tool_tips_page.navigate(test_data["tool_tips_url"])
+
+    tool_tips_page.hover_over_field()
+    
+    tooltip_text = tool_tips_page.get_tooltip_text()
+
+    assert tooltip_text == (test_data["tool_tip_field_text"])
